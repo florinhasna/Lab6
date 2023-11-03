a2633b8527420d25f4c46575eea92c2519ea96d8
Florin Hasna <florinhasna@outlook.com>
2023-11-03 10:46:31 +0000

    The program doesn't return anything yet, created a function to swap elements.

---
+++ ArraySwap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// a function that swaps the element on pos1 to pos2
+void swap_array(int array[], int pos1, int pos2);
+
+int main(){
+  int my_array[] = {1, 2, 3};
+  return 0;
+}
+
+void swap_array(int array[], int pos1, int pos2){
+  // hold the element on pos2 temporarily
+  int temp = array[pos2];
+
+  // swap elements
+  array[pos2] = array[pos1];
+  array[pos1] = temp;
+}
