e28941b9153a384a8d02b147511b00055f250324
Florin Hasna <florinhasna@outlook.com>
2023-11-03 09:31:27 +0000

    Added a function to calculate the cube of an integer.

---
+++ cube.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// returns cube of a number
+int cube(int aNumber){
+  int computedCube = aNumber * aNumber * aNumber;
+  return computedCube;
+}
+
+int main(){
+  return 0;
+}
