6c72904dc64a863ef61f9c89cf47754e3228ecaf
Florin Hasna <florinhasna@outlook.com>
2023-11-10 10:42:52 +0000

    Added a function to print the give arguments separated by a space

--- args.cpp
+++ args.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void print_args(char *args_array[], int size);
+
 int main(int argc, char *argv[]){
 
   cout << "The program name is: " << argv[0] << endl;
 
-  cout << "called with " << argc - 1 << " arguments: "; 
+  cout << "called with " << argc - 1 << " arguments: ";
+  print_args(argv, argc);
+  cout << endl;
   
   return 0;
+}
+
+void print_args(char *args_array[], int size){
+  for (int i = 1; i < size; i++){
+    cout << args_array[i] << " ";
+  }
 }
